23e17b12e4a68d11c18f9854ae7b48e5d23952fe
Simerpreet Singh <726010@student.tor.loc>
2019-04-18 14:24:59 -0400

    second commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,7 +14,31 @@ namespace ConsoleApp1
             Console.Write("Give the input of number of elements to be stored in the array :");
             n = Convert.ToInt32(Console.ReadLine());
 
-            //Console.Write("Input {0} elements in the array :\n", n); /*part1*/
+            Console.Write("Input {0} elements in the array :\n", n); /*part1*/
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                arr1[i] = Convert.ToInt32(Console.ReadLine());
+                fre1[i] = -1;
+            }
+            for (i = 0; i < n; i++)
+            {
+                ctr = 1;
+                for (j = i + 1; j < n; j++)
+                {
+                    if (arr1[i] == arr1[j])
+                    {
+                        ctr++;
+                        fre1[j] = 0;
+                    }
+                }
+
+                if (fre1[i] != 0)
+                {
+                    fre1[i] = ctr;
+                }
+            }
+            Console.Write("\nThe fre1 of all elements of the array : \n");/*part 2*/
         }
     }
 }
