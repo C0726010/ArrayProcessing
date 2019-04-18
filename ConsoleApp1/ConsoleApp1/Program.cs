8238b62028c182f63ac9af83ece527cc1542a472
Simerpreet Singh <726010@student.tor.loc>
2019-04-18 14:21:25 -0400

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[100];
+            int[] fre1 = new int[100];
+            int n, i, j, ctr;
+
+
+            Console.Write("Give the input of number of elements to be stored in the array :");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Input {0} elements in the array :\n", n); /*part1*/
+        }
+    }
+}
